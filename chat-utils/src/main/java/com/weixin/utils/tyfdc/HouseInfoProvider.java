0e2e9b15bab4443914c0b4d63bdb2f5b7f7e672c
guozhihua <guozhihua@e-eduspace.com>
2017-04-14 11:08:32 +0800

    多线程导入房产信息数据

--- chat-utils/src/main/java/com/weixin/utils/tyfdc/HouseInfoProvider.java
+++ chat-utils/src/main/java/com/weixin/utils/tyfdc/HouseInfoProvider.java
@@ -1,7 +1,13 @@
 package com.weixin.utils.tyfdc;
 
+import com.weixin.utils.jdbc.JdbcUtils;
 import com.weixin.utils.util.HttpRequest;
 import com.weixin.utils.util.JsonUtils;
+import org.apache.commons.dbutils.QueryRunner;
+import org.apache.commons.dbutils.handlers.BeanHandler;
+import org.apache.commons.dbutils.handlers.BeanListHandler;
+import org.apache.commons.dbutils.handlers.BeanMapHandler;
+import org.apache.commons.dbutils.handlers.MapListHandler;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -11,9 +17,13 @@ import org.jsoup.select.Elements;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 /**
  *http://wsfc.nsae.tyfdc.gov.cn/Firsthand/tyfc/publish/p/ProjInfo.do?propid=
@@ -34,8 +44,7 @@ public class HouseInfoProvider {
         //获取房产数据信息
 //        getHouseDataInfo("43139");
         //获取项目信息
-       getHousePrjInfo("43139");
-
+       new HouseInfoProvider().fillInPropertuesVal();
 
     }
 
@@ -133,30 +142,126 @@ public class HouseInfoProvider {
 
         return null;
     }
-    public static Map getHousePrjInfo(String id){
+
+    public  void fillInPropertuesVal(){
+        ExecutorService executor = Executors.newCachedThreadPool();
+
+        try{
+            QueryRunner queryRunner = new QueryRunner(JdbcUtils.getDataSource("hxs_wx"));
+            String countSql="select count(id) as totalCount from house_info";
+            List<Map<String, Object>> countList = queryRunner.query(countSql, new MapListHandler());
+            long totalCount = (long) countList.get(0).get("totalCount");
+            if(totalCount>0){
+                int pageSize =100;
+                long pages =totalCount/pageSize;
+                if(!(totalCount%pageSize==0)){
+                    pages++;
+                }
+                int maxThreadSize = Runtime.getRuntime().availableProcessors() - 1;//最多启用线程数
+                //根据每个线程处理2页来计算出来需要启用的线程数目
+                int threadSize = (int) (pages / 2) + 1;
+                if (threadSize > maxThreadSize) {
+                    threadSize = maxThreadSize;
+                }
+                long interval = (int) (pages / threadSize);
+
+                CountDownLatch latch = new CountDownLatch(threadSize);
+                for (int i = 1; i <= threadSize; i++) {
+                    long startPage = interval * (i - 1) + 1;
+                    long endPage = (i == threadSize) ? pages : startPage + interval - 1;
+                    String sql="select id from house_info";
+                    CourseStudyIndexThread thread = new CourseStudyIndexThread(latch, startPage, endPage, queryRunner, sql);
+                    executor.execute(thread);
+                }
+                executor.shutdown();
+            }
+        }catch (Exception ex){
+            executor.shutdown();
+            ex.printStackTrace();
+        }
+
+
+    }
+
+    class CourseStudyIndexThread extends CommonIndexThead {
+        public CourseStudyIndexThread() {
+        }
+
+        public CourseStudyIndexThread(CountDownLatch latch, long startPage, long endPage, QueryRunner runner, String sql) {
+            this.latch = latch;
+            this.startPage = startPage;
+            this.endPage = endPage;
+            this.runner = runner;
+            this.sql = sql;
+        }
+
+
+        @Override
+        public void run() {
+
+            try{
+                for (long j = this.startPage; j <= this.endPage; j++) {
+                    List<house> llis= (List<house>) runner.query(JdbcUtils.getPageSql(sql,(int)j,100),new BeanListHandler(house.class));
+                    for(house id :llis){
+                        setHousePrjInfo(id.getId());
+                    }
+                }
+                this.latch.countDown();
+                System.out.println("剩余线程数===========：" + this.latch.getCount());
+            }catch (Exception ex){
+
+            }
+
+        }
+    }
+
+    public static void setHousePrjInfo(String id){
         try {
+            QueryRunner queryRunner = new QueryRunner(JdbcUtils.getDataSource("hxs_wx"));
+            String sql="select * from house_info where id = "+id;
+           house house = (com.weixin.utils.tyfdc.house) queryRunner.query(sql,new BeanHandler(house.class));
+
             String rs=HttpRequest.getRequest(hosue_prj_info+id);
             Document document = Jsoup.parse(rs);
             Elements elements = document.getElementsByTag("tbody");
+            List<String> values = new ArrayList<>();
            Element tby=elements.get(0);
-            if(tby.childNodeSize()>0){
+            if(house!=null&&tby.childNodeSize()>0){
                 List<Node> nodes=tby.childNodes();
                 for(Node n:nodes){
                     if(n.childNodeSize()>0){
                         List<Node> nodes1=n.childNodes();
                         for(Node n1:nodes1){
                             if(n1.childNodeSize()>0){
-                                System.out.println(n1.childNode(0).toString());
+                                values.add(n1.childNode(0).toString().replace("&nbsp;",""));
                             }
                         }
                     }
                 }
             }
-                System.out.println(rs);
+           if(values.size()==48){
+               house.setAddress(values.get(5));
+               house.setMianji(values.get(15));
+               house.setStartTime(values.get(25));
+               house.setEndTime(values.get(27));
+               house.setNuan(values.get(29));
+               house.setQi(values.get(31));
+               house.setWater(values.get(33));
+               house.setJunjia(values.get(35));
+               house.setZhuangxiu(values.get(37));
+               house.setShigong(values.get(41));
+               house.setRemark(values.get(47));
+               String updateSql= "update house_info set startTime ='"+house.getStartTime()+"',endTime ='"+house.getEndTime()+"'," +
+                       "address ='"+house.getAddress()+"',mianji='"+house.getMianji()+"',zhuangxiu='"+house.getZhuangxiu()+"'," +
+                       "nuan='"+house.getNuan()+"',qi='"+house.getQi()+"',water='"+house.getWater()+"',junjia='"+house.getJunjia()+"'" +
+                       ",shigong='"+house.getShigong()+"',remark='"+house.getRemark()+"' where id = "+id;
+               int num =queryRunner.update(updateSql);
+           }
+
         }catch (Exception ex){
-            ex.printStackTrace();
+
+            System.out.println("id =【"+id+"】 更新失败"+ex.getMessage());
         }
-        return  null;
     }
 
     public static final String getHouseUnSaleInfo() {
@@ -164,24 +269,14 @@ public class HouseInfoProvider {
     }
 
 
-    class SimpleObj implements Serializable{
-        private String code ;
-        private  String name ;
-
-        public String getCode() {
-            return code;
-        }
-
-        public void setCode(String code) {
-            this.code = code;
-        }
-
-        public String getName() {
-            return name;
-        }
-
-        public void setName(String name) {
-            this.name = name;
+    public static final void getList(){
+        try{
+            QueryRunner queryRunner = new QueryRunner(JdbcUtils.getDataSource("hxs_wx"));
+            String sql="select * from house_info limit 10";
+            List<house> maps = (List<house>) queryRunner.query(sql,new BeanListHandler(house.class));
+            System.out.println(JsonUtils.toJson(maps));
+        }catch (Exception ex){
+                   ex.printStackTrace();
         }
     }
 }
---
+++ chat-utils/src/main/java/com/weixin/utils/tyfdc/house.java
@@ -0,0 +1,190 @@
+package com.weixin.utils.tyfdc;
+
+import java.io.Serializable;
+
+/**
+ *
+ *
+ * `id` char(10) DEFAULT NULL COMMENT 'id',
+ `houseName` varchar(100) DEFAULT NULL COMMENT '项目名称',
+ `areaName` varchar(30) DEFAULT NULL COMMENT '区域名称',
+ `houseType` varchar(30) DEFAULT NULL COMMENT '房屋类型',
+ `yushouCode` varchar(40) DEFAULT NULL COMMENT '预售号',
+ `kaifashang` varchar(60) DEFAULT NULL COMMENT '开发商',
+ `startTime` varchar(20) DEFAULT NULL COMMENT '开始时间',
+ `endTime` varchar(20) DEFAULT NULL COMMENT '完工时间',
+ `address` varchar(60) DEFAULT NULL COMMENT '地址',
+ `mianji` varchar(10) DEFAULT NULL COMMENT '销售面积',
+ `zhuangxiu` varchar(20) DEFAULT NULL COMMENT '装修',
+ `nuan` varchar(20) DEFAULT NULL COMMENT '供暖',
+ `qi` varchar(20) DEFAULT NULL COMMENT '天然气',
+ `water` varchar(20) DEFAULT NULL COMMENT '供水',
+ `junjia` varchar(20) DEFAULT NULL COMMENT '均价',
+ `shigong` varchar(60) DEFAULT NULL COMMENT '施工
+ * Created by :Guozhihua
+ * Date： 2017/4/14.
+ */
+public class house implements Serializable {
+    private int pid;
+    private String id;
+    private String houseName;
+    private String areaName;
+    private String houseType;
+    private String yushouCode;
+    private String kaifashang;
+    private String startTime;
+    private String endTime;
+    private String address;
+    private String mianji;
+    private String zhuangxiu;
+    private String nuan;
+    private String qi;
+    private String water;
+    private String junjia;
+    private String shigong;
+    private String remark;
+
+    public int getPid() {
+        return pid;
+    }
+
+    public void setPid(int pid) {
+        this.pid = pid;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getHouseName() {
+        return houseName;
+    }
+
+    public void setHouseName(String houseName) {
+        this.houseName = houseName;
+    }
+
+    public String getAreaName() {
+        return areaName;
+    }
+
+    public void setAreaName(String areaName) {
+        this.areaName = areaName;
+    }
+
+    public String getHouseType() {
+        return houseType;
+    }
+
+    public void setHouseType(String houseType) {
+        this.houseType = houseType;
+    }
+
+    public String getYushouCode() {
+        return yushouCode;
+    }
+
+    public void setYushouCode(String yushouCode) {
+        this.yushouCode = yushouCode;
+    }
+
+    public String getKaifashang() {
+        return kaifashang;
+    }
+
+    public void setKaifashang(String kaifashang) {
+        this.kaifashang = kaifashang;
+    }
+
+    public String getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(String startTime) {
+        this.startTime = startTime;
+    }
+
+    public String getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(String endTime) {
+        this.endTime = endTime;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getMianji() {
+        return mianji;
+    }
+
+    public void setMianji(String mianji) {
+        this.mianji = mianji;
+    }
+
+    public String getZhuangxiu() {
+        return zhuangxiu;
+    }
+
+    public void setZhuangxiu(String zhuangxiu) {
+        this.zhuangxiu = zhuangxiu;
+    }
+
+    public String getNuan() {
+        return nuan;
+    }
+
+    public void setNuan(String nuan) {
+        this.nuan = nuan;
+    }
+
+    public String getQi() {
+        return qi;
+    }
+
+    public void setQi(String qi) {
+        this.qi = qi;
+    }
+
+    public String getWater() {
+        return water;
+    }
+
+    public void setWater(String water) {
+        this.water = water;
+    }
+
+    public String getJunjia() {
+        return junjia;
+    }
+
+    public void setJunjia(String junjia) {
+        this.junjia = junjia;
+    }
+
+    public String getShigong() {
+        return shigong;
+    }
+
+    public void setShigong(String shigong) {
+        this.shigong = shigong;
+    }
+
+    public String getRemark() {
+        return remark;
+    }
+
+    public void setRemark(String remark) {
+        this.remark = remark;
+    }
+}
