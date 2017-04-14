c09bd413b75897fabb6fbb20855678855e52dba5
guozhihua <guozhihua@e-eduspace.com>
2017-04-14 11:58:09 +0800

    多线程导入房产信息数据

--- chat-utils/src/main/java/com/weixin/utils/tyfdc/HouseInfoProvider.java
+++ chat-utils/src/main/java/com/weixin/utils/tyfdc/HouseInfoProvider.java
@@ -4,7 +4,9 @@ import com.weixin.utils.jdbc.JdbcUtils;
 import com.weixin.utils.util.CommonIndexThead;
 import com.weixin.utils.util.HttpRequest;
 import com.weixin.utils.util.JsonUtils;
+import org.apache.commons.dbutils.BaseResultSetHandler;
 import org.apache.commons.dbutils.QueryRunner;
+import org.apache.commons.dbutils.ResultSetHandler;
 import org.apache.commons.dbutils.handlers.BeanHandler;
 import org.apache.commons.dbutils.handlers.BeanListHandler;
 import org.apache.commons.dbutils.handlers.MapListHandler;
@@ -25,15 +27,16 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 /**
- *http://wsfc.nsae.tyfdc.gov.cn/Firsthand/tyfc/publish/p/ProjInfo.do?propid=
+ * http://wsfc.nsae.tyfdc.gov.cn/Firsthand/tyfc/publish/p/ProjInfo.do?propid=
  * Created by :Guozhihua
  * Date： 2017/4/13.
  */
 public class HouseInfoProvider {
     private static final String houseList_url = "http://wsfc.nsae.tyfdc.gov.cn/Firsthand/tyfc/publish/ProjListForPassed.do";
 
-    private static final String hosue_data_info ="http://wsfc.nsae.tyfdc.gov.cn/Firsthand/tyfc/publish/ProNBList.do";
-    private static final String hosue_prj_info ="http://wsfc.nsae.tyfdc.gov.cn/Firsthand/tyfc/publish/p/ProjInfo.do?propid=";
+    private static final String hosue_data_info = "http://wsfc.nsae.tyfdc.gov.cn/Firsthand/tyfc/publish/ProNBList.do";
+    private static final String hosue_prj_info = "http://wsfc.nsae.tyfdc.gov.cn/Firsthand/tyfc/publish/p/ProjInfo.do?propid=";
+
     public static void getHoueseList(String url, Map<String, Object> params) {
         String respose = HttpRequest.postRequest(url, JsonUtils.toJson(params));
 
@@ -41,10 +44,10 @@ public class HouseInfoProvider {
 
     public static void main(String[] args) {
         //获取房产数据信息
-//        getHouseDataInfo("43139");
-        //获取项目信息
-       new HouseInfoProvider().fillInPropertuesVal();
-
+        new HouseInfoProvider().setHouseDataInfo(getQueryRunner(),"43139");
+        //补充房子信息
+//       new HouseInfoProvider().fillInPropertuesVal();
+        //获取房子具体是否售出信息
     }
 
 
@@ -61,11 +64,82 @@ public class HouseInfoProvider {
      * pageNo:1
      * pageSize:15
      */
-    private static String getHouseList(Map<String, Object> paramMap) {
+//    private static String getHouseList(Map<String, Object> paramMap) {
+//        try {
+//            Map<String, Object> params = new HashMap<>();
+//            params.put("ProName", "");
+//
+//            params.put("pageSize", 1000);
+//            params.put("PerYear", "");
+//            params.put("PerFlowNO", "");
+//            params.put("OrgName", "");
+//            params.put("ProAddress", "");
+//            params.put("ProType", 0);
+//            params.put("PerType", 0);
+//            params.put("HouseType", 0);
+//            params.put("Region", 0);
+//            StringBuilder sb = new StringBuilder();
+//
+//            for (int i = 0; i < 5; i++) {
+//                params.put("pageNo", i + 1);
+//                String respose = HttpRequest.postFormRequest(houseList_url, params);
+//                if (!respose.contains("objid")) {
+//                    continue;
+//                }
+//                Document document = Jsoup.parse(respose);
+//                Elements elements = document.getElementsByAttribute("objid");
+//                for (Element element : elements) {
+//                    String id = element.attributes().get("objid");
+//                    List<Node> nodes = element.childNodes();
+//                    String houseName = nodes.get(2).childNode(0).childNode(0).childNode(0).toString();
+//                    try {
+//                        String areaName = nodes.get(4).childNode(0).toString();
+//                        String houseType = "";
+//                        if (nodes.get(6).childNodeSize() > 0) {
+//                            houseType = nodes.get(6).childNode(0).toString();
+//                        }
+//                        String yushou = nodes.get(8).childNode(0).toString();
+//                        String kaifa = nodes.get(10).childNode(0).childNode(0).childNode(0).toString();
+//                        sb.append("insert into house_info(id,hosueName,areaName,houseType,yushouCode,kaifashang)" +
+//                                " valuse('" + id + "','" + houseName + "','" + areaName + "','" + houseType + "','" + yushou + "','" + kaifa + "');").append("\r\n");
+//                    } catch (Exception ex) {
+//                        System.out.println("id:" + id + "name:" + houseName + "=============================");
+//                        continue;
+//                    }
+//                }
+//            }
+//
+//            File f = new File("d:\\test.sql");
+//            if (!f.exists()) {
+//                f.createNewFile();
+//            }
+//            FileWriter fw = new FileWriter(f);
+//            fw.write(sb.toString());
+//            fw.flush();
+//            fw.close();
+//        } catch (Exception ex) {
+//            ex.printStackTrace();
+//        }
+//        return null;
+//    }
+
+    /**
+     * PerYear:
+     * PerFlowNO:
+     * PerType:0
+     * ProType:0
+     * ProName:东
+     * OrgName:
+     * HouseType:0
+     * Region:0
+     * ProAddress:
+     * pageNo:1
+     * pageSize:15
+     */
+    private static String getAllHouseList(Map<String, Object> paramMap) {
         try {
             Map<String, Object> params = new HashMap<>();
             params.put("ProName", "");
-
             params.put("pageSize", 1000);
             params.put("PerYear", "");
             params.put("PerFlowNO", "");
@@ -76,11 +150,11 @@ public class HouseInfoProvider {
             params.put("HouseType", 0);
             params.put("Region", 0);
             StringBuilder sb = new StringBuilder();
-
-            for(int i= 0;i<5;i++){
-                params.put("pageNo", i+1);
+            QueryRunner queryRunner=getQueryRunner();
+            for (int i = 0; i < 5; i++) {
+                params.put("pageNo", i + 1);
                 String respose = HttpRequest.postFormRequest(houseList_url, params);
-                if(!respose.contains("objid")){
+                if (!respose.contains("objid")) {
                     continue;
                 }
                 Document document = Jsoup.parse(respose);
@@ -91,29 +165,24 @@ public class HouseInfoProvider {
                     String houseName = nodes.get(2).childNode(0).childNode(0).childNode(0).toString();
                     try {
                         String areaName = nodes.get(4).childNode(0).toString();
-                        String houseType ="";
-                        if(nodes.get(6).childNodeSize()>0){
+                        String houseType = "";
+                        if (nodes.get(6).childNodeSize() > 0) {
                             houseType = nodes.get(6).childNode(0).toString();
                         }
                         String yushou = nodes.get(8).childNode(0).toString();
                         String kaifa = nodes.get(10).childNode(0).childNode(0).childNode(0).toString();
                         sb.append("insert into house_info(id,hosueName,areaName,houseType,yushouCode,kaifashang)" +
                                 " valuse('" + id + "','" + houseName + "','" + areaName + "','" + houseType + "','" + yushou + "','" + kaifa + "');").append("\r\n");
-                    }catch (Exception ex){
-                        System.out.println("id:"+id+"name:"+houseName+"=============================");
+                        queryRunner.insert(sb.toString(), new BeanHandler(String.class) {
+                        });
+                    } catch (Exception ex) {
+                        System.out.println("id:" + id + "name:" + houseName + "=============================");
                         continue;
                     }
                 }
             }
 
-            File f = new File("d:\\test.sql");
-            if(!f.exists()){
-                f.createNewFile();
-            }
-            FileWriter fw =new FileWriter(f);
-            fw.write(sb.toString());
-            fw.flush();
-            fw.close();
+
         } catch (Exception ex) {
             ex.printStackTrace();
         }
@@ -121,20 +190,43 @@ public class HouseInfoProvider {
     }
 
     /**
-     * 房产数据信息
+     * 房产数据信息，第一步，获取到具体的楼
+     *                第二步，获取每个楼的出售情况和户型的具体数据
      *
-     * @param id
+     * @param pid
      * @return
      */
-    public static String getHouseDataInfo(String id) {
-        try{
+    public String setHouseDataInfo( QueryRunner queryRunner,String pid) {
+        try {
             Map<String, Object> params = new HashMap<>();
-            params.put("pid", id);
-            params.put("pageSize", 100);
+            params.put("pid", pid);
+            params.put("pageSize", 50);
             params.put("pageNo", 1);
-            String rs =HttpRequest.postFormRequest(hosue_data_info,params);
-            System.out.println(rs);
-        }catch (Exception ex){
+            String respose = HttpRequest.postFormRequest(hosue_data_info, params);
+            if (!respose.contains("objid")) {
+                return null;
+            }
+            Document document = Jsoup.parse(respose);
+            //有多少objid,就有多少行
+            Elements elements = document.getElementsByAttribute("objid");
+            String insertSql="insert into house_obj(nid,houseId,houseName,remark,objId) values(?,?,?,?,?)";
+            Object sqlParams[][] = new Object[elements.size()][];
+            int i =0;
+            for (Element element : elements) {
+                String id = element.attributes().get("objid");
+                List<Node> nodes = element.childNodes();
+                try {
+                   //在里面把数据抽取出来，sql参数数组里
+                    sqlParams[i] = new Object[] { "aa" , "123", "aa@sina.com","",""};
+                    i++;
+                } catch (Exception ex) {
+                    System.out.println("id:" +id );
+                    continue;
+                }
+            }
+            //批量插入数据
+           queryRunner.batch(insertSql,sqlParams);
+        } catch (Exception ex) {
             ex.printStackTrace();
         }
 
@@ -142,18 +234,18 @@ public class HouseInfoProvider {
         return null;
     }
 
-    public  void fillInPropertuesVal(){
+    public void fillInPropertuesVal() {
         ExecutorService executor = Executors.newCachedThreadPool();
 
-        try{
+        try {
             QueryRunner queryRunner = new QueryRunner(JdbcUtils.getDataSource("hxs_wx"));
-            String countSql="select count(id) as totalCount from house_info";
+            String countSql = "select count(id) as totalCount from house_info";
             List<Map<String, Object>> countList = queryRunner.query(countSql, new MapListHandler());
             long totalCount = (long) countList.get(0).get("totalCount");
-            if(totalCount>0){
-                int pageSize =100;
-                long pages =totalCount/pageSize;
-                if(!(totalCount%pageSize==0)){
+            if (totalCount > 0) {
+                int pageSize = 100;
+                long pages = totalCount / pageSize;
+                if (!(totalCount % pageSize == 0)) {
                     pages++;
                 }
                 int maxThreadSize = Runtime.getRuntime().availableProcessors() - 1;//最多启用线程数
@@ -168,13 +260,13 @@ public class HouseInfoProvider {
                 for (int i = 1; i <= threadSize; i++) {
                     long startPage = interval * (i - 1) + 1;
                     long endPage = (i == threadSize) ? pages : startPage + interval - 1;
-                    String sql="select id from house_info";
+                    String sql = "select id from house_info";
                     CourseStudyIndexThread thread = new CourseStudyIndexThread(latch, startPage, endPage, queryRunner, sql);
                     executor.execute(thread);
                 }
                 executor.shutdown();
             }
-        }catch (Exception ex){
+        } catch (Exception ex) {
             executor.shutdown();
             ex.printStackTrace();
         }
@@ -197,85 +289,84 @@ public class HouseInfoProvider {
 
         @Override
         public void run() {
-
-            try{
+            try {
                 for (long j = this.startPage; j <= this.endPage; j++) {
-                    List<house> llis= (List<house>) runner.query(JdbcUtils.getPageSql(sql,(int)j,100),new BeanListHandler(house.class));
-                    for(house id :llis){
+                    List<house> llis = (List<house>) runner.query(JdbcUtils.getPageSql(sql, (int) j, 100), new BeanListHandler(house.class));
+                    for (house id : llis) {
                         setHousePrjInfo(id.getId());
                     }
                 }
                 this.latch.countDown();
                 System.out.println("剩余线程数===========：" + this.latch.getCount());
-            }catch (Exception ex){
+            } catch (Exception ex) {
 
             }
 
         }
     }
 
-    public static void setHousePrjInfo(String id){
+
+    /**
+     * 填充单个房子的信息
+     *
+     * @param id
+     */
+    public static void setHousePrjInfo(String id) {
         try {
             QueryRunner queryRunner = new QueryRunner(JdbcUtils.getDataSource("hxs_wx"));
-            String sql="select * from house_info where id = "+id;
-           house house = (com.weixin.utils.tyfdc.house) queryRunner.query(sql,new BeanHandler(house.class));
+            String sql = "select * from house_info where id = " + id;
+            house house = (com.weixin.utils.tyfdc.house) queryRunner.query(sql, new BeanHandler(house.class));
 
-            String rs=HttpRequest.getRequest(hosue_prj_info+id);
+            String rs = HttpRequest.getRequest(hosue_prj_info + id);
             Document document = Jsoup.parse(rs);
             Elements elements = document.getElementsByTag("tbody");
             List<String> values = new ArrayList<>();
-           Element tby=elements.get(0);
-            if(house!=null&&tby.childNodeSize()>0){
-                List<Node> nodes=tby.childNodes();
-                for(Node n:nodes){
-                    if(n.childNodeSize()>0){
-                        List<Node> nodes1=n.childNodes();
-                        for(Node n1:nodes1){
-                            if(n1.childNodeSize()>0){
-                                values.add(n1.childNode(0).toString().replace("&nbsp;",""));
+            Element tby = elements.get(0);
+            if (house != null && tby.childNodeSize() > 0) {
+                List<Node> nodes = tby.childNodes();
+                for (Node n : nodes) {
+                    if (n.childNodeSize() > 0) {
+                        List<Node> nodes1 = n.childNodes();
+                        for (Node n1 : nodes1) {
+                            if (n1.childNodeSize() > 0) {
+                                values.add(n1.childNode(0).toString().replace("&nbsp;", ""));
                             }
                         }
                     }
                 }
             }
-           if(values.size()==48){
-               house.setAddress(values.get(5));
-               house.setMianji(values.get(15));
-               house.setStartTime(values.get(25));
-               house.setEndTime(values.get(27));
-               house.setNuan(values.get(29));
-               house.setQi(values.get(31));
-               house.setWater(values.get(33));
-               house.setJunjia(values.get(35));
-               house.setZhuangxiu(values.get(37));
-               house.setShigong(values.get(41));
-               house.setRemark(values.get(47));
-               String updateSql= "update house_info set startTime ='"+house.getStartTime()+"',endTime ='"+house.getEndTime()+"'," +
-                       "address ='"+house.getAddress()+"',mianji='"+house.getMianji()+"',zhuangxiu='"+house.getZhuangxiu()+"'," +
-                       "nuan='"+house.getNuan()+"',qi='"+house.getQi()+"',water='"+house.getWater()+"',junjia='"+house.getJunjia()+"'" +
-                       ",shigong='"+house.getShigong()+"',remark='"+house.getRemark()+"' where id = "+id;
-               int num =queryRunner.update(updateSql);
-           }
+            if (values.size() == 48) {
+                house.setAddress(values.get(5));
+                house.setMianji(values.get(15));
+                house.setStartTime(values.get(25));
+                house.setEndTime(values.get(27));
+                house.setNuan(values.get(29));
+                house.setQi(values.get(31));
+                house.setWater(values.get(33));
+                house.setJunjia(values.get(35));
+                house.setZhuangxiu(values.get(37));
+                house.setShigong(values.get(41));
+                house.setRemark(values.get(47));
+                String updateSql = "update house_info set startTime ='" + house.getStartTime() + "',endTime ='" + house.getEndTime() + "'," +
+                        "address ='" + house.getAddress() + "',mianji='" + house.getMianji() + "',zhuangxiu='" + house.getZhuangxiu() + "'," +
+                        "nuan='" + house.getNuan() + "',qi='" + house.getQi() + "',water='" + house.getWater() + "',junjia='" + house.getJunjia() + "'" +
+                        ",shigong='" + house.getShigong() + "',remark='" + house.getRemark() + "' where id = " + id;
+                int num = queryRunner.update(updateSql);
+            }
 
-        }catch (Exception ex){
+        } catch (Exception ex) {
 
-            System.out.println("id =【"+id+"】 更新失败"+ex.getMessage());
+            System.out.println("id =【" + id + "】 更新失败" + ex.getMessage());
         }
     }
 
-    public static final String getHouseUnSaleInfo() {
-        return null;
+    public void setHouseUnSaleInfo(String id) {
+
     }
 
-
-    public static final void getList(){
-        try{
-            QueryRunner queryRunner = new QueryRunner(JdbcUtils.getDataSource("hxs_wx"));
-            String sql="select * from house_info limit 10";
-            List<house> maps = (List<house>) queryRunner.query(sql,new BeanListHandler(house.class));
-            System.out.println(JsonUtils.toJson(maps));
-        }catch (Exception ex){
-                   ex.printStackTrace();
-        }
+    private static QueryRunner getQueryRunner(){
+        QueryRunner queryRunner = new QueryRunner(JdbcUtils.getDataSource("hxs_wx"));
+        return queryRunner;
     }
+
 }
---
+++ chat-utils/src/main/java/com/weixin/utils/tyfdc/houseObj.java
@@ -0,0 +1,55 @@
+package com.weixin.utils.tyfdc;
+
+import java.io.Serializable;
+
+/**
+ * Created by :Guozhihua
+ * Date： 2017/4/14.
+ */
+public class houseObj implements Serializable {
+    private  int nid;
+    private String houseId;
+    private String houseName;
+    private String remark;
+    private String objId;
+
+    public int getNid() {
+        return nid;
+    }
+
+    public void setNid(int nid) {
+        this.nid = nid;
+    }
+
+    public String getHouseId() {
+        return houseId;
+    }
+
+    public void setHouseId(String houseId) {
+        this.houseId = houseId;
+    }
+
+    public String getHouseName() {
+        return houseName;
+    }
+
+    public void setHouseName(String houseName) {
+        this.houseName = houseName;
+    }
+
+    public String getRemark() {
+        return remark;
+    }
+
+    public void setRemark(String remark) {
+        this.remark = remark;
+    }
+
+    public String getObjId() {
+        return objId;
+    }
+
+    public void setObjId(String objId) {
+        this.objId = objId;
+    }
+}
