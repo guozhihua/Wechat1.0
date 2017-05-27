6798fd32c6b488e4befa801e20ac37c71755e564
guozhihua <guozhihua@e-eduspace.com>
2017-05-27 12:04:18 +0800

    dubbox  框架的使用

--- chat-sso/src/main/java/com/chat/sso/provider/SsoUserLoginServiceImpl.java
+++ chat-sso/src/main/java/com/chat/sso/provider/SsoUserLoginServiceImpl.java
@@ -2,6 +2,10 @@ package com.chat.sso.provider;
 
 import com.weixin.entity.chat.User;
 import sso.provider.SsoUserLoginService;
+import sun.net.NetworkClient;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 /**
  * Created by :Guozhihua
@@ -12,6 +16,11 @@ public class SsoUserLoginServiceImpl implements SsoUserLoginService {
     public User queryUserByTicket(String ticket) {
         User user = new User();
         user.setUserName("zhang san");
+        try {
+            System.out.println("sso test..host is "+ InetAddress.getLocalHost().getHostAddress());
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
         return user;
     }
 }
