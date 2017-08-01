448deed2f40d5b1dc968b4537560ad171dc3d1f1
guozhihua <guozhihua@e-eduspace.com>
2017-08-01 18:02:38 +0800

    修改模板

--- OAuth2/src/test/java/Test.java
+++ OAuth2/src/test/java/Test.java
@@ -1,6 +1,14 @@
-import org.springframework.util.StringUtils;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
 
-import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by :Guozhihua
@@ -8,10 +16,43 @@ import java.lang.reflect.Array;
  */
 public class Test {
 
-    public static void main(String[] args) {
-        String[] array= new String[]{"123123","12312"};
-        System.out.println(StringUtils.arrayToCommaDelimitedString(array));
+    public static void main(String[] args)  {
+        String  urkl ="http://pod.dsylove.com/user/nextUserDetail";
+        // 创建默认的httpClient实例.
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+        // 创建httppost
+        CloseableHttpResponse response =null;
+        List<String> res=new ArrayList<>();
+        try {
+            HttpPost httppost = new HttpPost(urkl);
+            httppost.addHeader("token", "402167221");
+            httppost.addHeader("version", "3.3.3");
+            httppost.addHeader("Content-Type", "application/x-www-form-urlencoded");
+            for(int i=0;i<1000;i++){
+                response = httpclient.execute(httppost);
+                HttpEntity entity = response.getEntity();
+                if (entity != null) {
+                    JSONObject jsonObject = JSON.parseObject(EntityUtils.toString(entity, "UTF-8")).getJSONObject("data");
+                    if(jsonObject!=null&&jsonObject.containsKey("weixin")&&!jsonObject.getString("weixin").equals("对方未设置或不可见")){
+                        String s ="姓名："+jsonObject.getString("nickName")+"   年龄："+jsonObject.getString("info1")+"  微信："+jsonObject.getString("weixin");
+                        res.add(s);
+                    }
+                }
+                response.close();
 
+            }
+
+        }catch (Exception ex){
+            ex.printStackTrace();
+
+        }finally {
+            System.out.println("--------------------------------------");
+            for(String s :res){
+                System.out.println(s);
+            }
+        }
 
     }
+
+
 }
