56b6890ecafe52b3f5d5100525f6849779badbf3
guozhihua <guozhihua@e-eduspace.com>
2018-01-15 09:12:59 +0800

    xg111

--- xchat/src/main/java/xchat/controller/CoderController.java
+++ xchat/src/main/java/xchat/controller/CoderController.java
@@ -42,4 +42,26 @@ public class CoderController extends  ABaseController{
         return webModel;
     }
 
+
+    @RequestMapping("/xg")
+    @ResponseBody
+    public WebModel xg(@RequestParam("params") String params) {
+        WebModel webModel = WebModel.getInstance();
+        try {
+            Map<String,Object> paramsMap =super.getParamMap();
+            String code =paramsMap.get("code").toString();
+            int size =Integer.parseInt(paramsMap.get("size").toString());
+            int fistCode =Integer.parseInt(paramsMap.get("fistCode").toString());
+            int last =ZhiShiCode.setCode(code,size,fistCode);
+            webModel.setDatas(last);
+            webModel.setMsg("成功充值"+last+"张复活卡");
+            logger.info("code is {},size is{},result is  {}",code,size,last);
+        } catch (Exception ex) {
+            logger.error("error", ex);
+            webModel.isFail();
+        }
+        return webModel;
+    }
+
+
 }
