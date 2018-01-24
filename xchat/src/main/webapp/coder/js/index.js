d1044dd04a1c4536f2b73ca82e0baab4a6e1110f
guozhihua <guozhihua@e-eduspace.com>
2018-01-24 14:42:06 +0800

    绑定回车时间

--- xchat/src/main/webapp/coder/js/index.js
+++ xchat/src/main/webapp/coder/js/index.js
@@ -14,3 +14,28 @@ function login() {
 
     }, "JSON");
 }
+
+//全局回车
+//$(document).keypress(function(e) {
+//    var eCode = e.keyCode ? e.keyCode : e.which ? e.which : e.charCode;
+//    if (eCode == 13){
+//        alert('您按了回车键')
+//        //自己写判断操作
+//    }
+//});
+
+
+$(document).ready(function () {
+    //回车提交事件
+    $("#username").keypress(function(event) {
+        if (event.keyCode == "13") {//keyCode=13是回车键
+            $("#loginBtn").click();
+        }
+    });
+
+    $("#password").keypress(function(event) {
+        if (event.keyCode == "13") {//keyCode=13是回车键
+            $("#loginBtn").click();
+        }
+    });
+});
